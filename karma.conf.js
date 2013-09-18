b7c6def1a6b6a405f9e7e6b9337019be7cdb8b4e
Johan Brissmyr <brissmyr@gmail.com>
2013-09-18 15:56:13 +0200

    chore: update Karma config to new format

--- karma.conf.js
+++ karma.conf.js
@@ -1,63 +1,80 @@
 // Karma configuration
+// Generated on Wed Sep 18 2013 15:45:48 GMT+0200 (CEST)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/components/jquery/jquery.js',
-  'app/components/angular-unstable/angular.js',
-  'app/components/angular-ui-bootstrap/src/dialog/dialog.js',
-  'app/components/angular-ui-bootstrap/src/transition/transition.js',
-  'app/components/angular-local-storage/angular-local-storage.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/components/codemirror/lib/codemirror.js',
-  'app/components/angular-ui-codemirror/ui-codemirror.js',
-  'app/components/d3/d3.js',
-  'app/components/UUID.js/src/uuid.js',
-  'app/scripts/*.coffee',
-  'app/scripts/**/*.coffee',
-  'test/spec/**/*.coffee'
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [];
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-// web server port
-port = 8080;
 
-// cli runner port
-runnerPort = 9100;
+    // list of files / patterns to load in the browser
+    files: [
+      'app/components/jquery/jquery.js',
+      'app/components/angular-unstable/angular.js',
+      'app/components/angular-ui-bootstrap/src/dialog/dialog.js',
+      'app/components/angular-ui-bootstrap/src/transition/transition.js',
+      'app/components/angular-local-storage/angular-local-storage.js',
+      'app/components/angular-mocks/angular-mocks.js',
+      'app/components/codemirror/lib/codemirror.js',
+      'app/components/angular-ui-codemirror/ui-codemirror.js',
+      'app/components/d3/d3.js',
+      'app/components/UUID.js/src/uuid.js',
+      'app/scripts/*.coffee',
+      'app/scripts/**/*.coffee',
+      'test/spec/**/*.coffee'
+    ],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // list of files to exclude
+    exclude: [
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    ],
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+
+    // web server port
+    port: 8080,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
