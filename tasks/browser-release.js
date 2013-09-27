56b15cbaba92ce93af2328ee84fd04c40feea897
Andreas Kollegger <akollegger@gmail.com>
2013-09-27 12:41:37 +0200

    updated release process, grunt now does most of the work, with maven just deploying the artifact

--- tasks/browser-release.js
+++ tasks/browser-release.js
@@ -12,7 +12,7 @@ var shell = require('shelljs');
 var semver = require('semver');
 
 module.exports = function(grunt){
-  grunt.registerTask('release', 'bump version, publish with mvn', function(type){
+  grunt.registerTask('release', 'bump version, deploy with mvn', function(type){
     //defaults
     var options = this.options({
       bump: true,
@@ -32,50 +32,55 @@ module.exports = function(grunt){
     var config = setup(options.file, type);
     var templateOptions = {
       data: {
-        version: config.newVersion
+        version: config.releaseVersion
       }
     };
 
-    if (options.bump) bump(config);
-    if (options.add) add(config);
-    if (options.commit) commit(config);
+    bump(config, config.releaseVersion);
+    add(config);
+    commit(config);
 
-    prepareMavenRelease(config);
+    // prepareMavenRelease(config);
 
-    performMavenRelease(config);
+    // performMavenRelease(config);
 
-    // if (options.tag) tag(config);
-    // if (options.push) push();
-    // if (options.pushTags) pushTags(config);
-    // if (options.publish) publish(config);
+    tag(config);
+    push();
+    pushTags(config);
+    publish(config);
 
+    bump(config, config.developmentVersion)
+    add(config)
+    commit(config)
 
     function setup(file, type){
       var pkg = grunt.file.readJSON(file);
-      var newVersion = pkg.version;
-      var nextVersion = semver.inc(pkg.version, type || 'patch');
+      var releaseVersion = pkg.version;
+      var developmentVersion = semver.inc(pkg.version, type || 'patch');
       if (options.bump) {
-        newVersion = semver.inc(pkg.version, type || 'patch');
-        nextVersion = semver.inc(newVersion, type || 'patch');
+        releaseVersion = semver.inc(pkg.version, type || 'patch');
+        developmentVersion = semver.inc(releaseVersion, type || 'patch') + "-SNAPSHOT";
       }
-      return {file: file, pkg: pkg, newVersion: newVersion, nextVersion: nextVersion };
+      return {file: file, pkg: pkg, releaseVersion: releaseVersion, developmentVersion: developmentVersion };
     }
 
-    // add updated npm package config to git
+    // add updated npm package config and maven pom to git
     function add(config){
       run('git add ' + config.file);
+      run('git add pom.xml')
     }
 
     // commit the updated npm package config
     function commit(config){
       var message = grunt.template.process(commitMessage, templateOptions);
-      run('git commit '+ config.file +' -m "'+ message +'"', config.file + ' committed');
+      run('git commit -m "'+ message+'"', 'package files committed');
     }
 
+    // updates local pom to match release version
     function prepareMavenRelease(config) {
-      mvn("mvn --batch-mode -Dtag=v"+config.newVersion+" release:prepare" +
-             " -DreleaseVersion="+config.newVersion +
-             " -DdevelopmentVersion="+config.nextVersion +"-SNAPSHOT"
+      mvn("mvn --batch-mode -Dtag=v"+config.releaseVersion+" release:update-versions" +
+             " -DreleaseVersion="+config.releaseVersion +
+             " -DdevelopmentVersion="+config.developmentVersion +"-SNAPSHOT"
       )
     }
 
@@ -94,24 +99,25 @@ module.exports = function(grunt){
     }
 
     function pushTags(config){
-      run('git push --tags', 'pushed new tag '+ config.newVersion +' to remote');
+      run('git push --tags', 'pushed new tag '+ config.releaseVersion +' to remote');
     }
 
-    function publish(config){
-      var cmd = 'npm publish';
-      var msg = 'published '+ config.newVersion +' to npm';
-      var npmtag = getNpmTag();
-      if (npmtag){ 
-        cmd += ' --tag ' + npmtag;
-        msg += ' with a tag of "' + npmtag + '"';
-      }
-      if (options.folder){ cmd += ' ' + options.folder }
-      run(cmd, msg);
+    function publish(config) {
+      mvn("mvn --batch-mode deploy")
+      // var cmd = 'npm publish';
+      // var msg = 'published '+ config.releaseVersion +' to npm';
+      // var npmtag = getNpmTag();
+      // if (npmtag){ 
+      //   cmd += ' --tag ' + npmtag;
+      //   msg += ' with a tag of "' + npmtag + '"';
+      // }
+      // if (options.folder){ cmd += ' ' + options.folder }
+      // run(cmd, msg);
     }
 
     function getNpmTag(){
       var tag = grunt.option('npmtag') || options.npmtag;
-      if(tag === true) { tag = config.newVersion }
+      if(tag === true) { tag = config.releaseVersion }
       return tag;
     }
 
@@ -143,10 +149,13 @@ module.exports = function(grunt){
       if (msg) grunt.log.ok(msg);
     }
 
-    function bump(config){
-      config.pkg.version = config.newVersion;
+    function bump(config, version) {
+      config.pkg.version = version
       grunt.file.write(config.file, JSON.stringify(config.pkg, null, '  ') + '\n');
-      grunt.log.ok('Version bumped to ' + config.newVersion);
+      mvn("mvn --batch-mode release:update-versions" +
+             " -DdevelopmentVersion="+version
+             )
+      grunt.log.ok('Versions bumped to ' + version);
     }
 
   });
