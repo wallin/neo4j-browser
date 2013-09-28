126814b0396bb58f160e0a4105fc3ff6dc41d996
Sebastian Wallin <sebastian.wallin@gmail.com>
2013-09-28 17:36:16 +0200

    test(e2e): setup basic e2e test structure. (not working correctly though...)

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,51 +1,73 @@
-// Karma E2E configuration
+// Karma configuration
+// Generated on Sat Sep 28 2013 16:50:42 GMT+0200 (CEST)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  'test/e2e/**/*.coffee'
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [];
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // frameworks to use
+    frameworks: ['ng-scenario'],
 
-// web server port
-port = 8080;
 
-// cli runner port
-runnerPort = 9100;
+    // list of files / patterns to load in the browser
+    files: [
+      'test/e2e/*.coffee'
+    ],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // list of files to exclude
+    exclude: [
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    ],
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+
+    // web server port
+    port: 9876,
+
+    proxies: {
+      "/": "http://localhost:9000/"
+    },
+
+    urlRoot: "/__karma/",
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
