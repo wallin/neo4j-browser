da6549fe9590c898f791bd19afdf48f73269b71d
Alistair Jones <alistair.jones@gmail.com>
2013-09-30 14:41:03 +0100

    Add junit reporting from Karma so that we can diagnose build problems.

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+    reporters: ['progress', 'junit'],
 
 
     // web server port
